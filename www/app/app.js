4d3b0b9456b1668af9449ce4bb4e69d969bf3f01
Rafael Khan <raf.khan.f@gmail.com>
2014-04-24 13:47:56 -0400

    Started application with test images. Also added lodash

--- www/app/app.js
+++ www/app/app.js
@@ -1,6 +1,39 @@
 angular.module('trance', [])
 
   .controller('MainCtrl', function($scope) {
-    $scope.msg = ":)";
+
+    var colorThief = new ColorThief();
+
+
+    $scope.tracks = [
+      {
+        name: 'Master of Puppets',
+        artist: 'Metallica',
+        album: 'Master of Puppets',
+        art:   'assets/img/test/master_of_puppets.jpg'
+      },
+
+      {
+        name: 'Eiforya (Radio Edit)',
+        artist: 'Armin van Buuren & Andrew Rayel',
+        album: 'A State of Trance 2014 - Mixed by Armin van Buuren (In the Club)',
+        art:   'assets/img/test/armin_asot.jpg'
+      }
+    ];
+
+    $scope.current = $scope.tracks[1];
+
+
+    $scope.setTrack = function(track) {
+      $scope.current = track;
+      var img = document.getElementById('current-art');
+      var palette = colorThief.getPalette(img, 4);
+
+      $scope.palette = _.map(palette, function(color) {
+        return ['rgb(', color[0], ',', color[1], ',', color[2], ')'].join('');
+      });
+    };
+
+    
   });
 
