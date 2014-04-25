dc7def05a34270962be550268fe1a0bad2e100d7
Rafael Khan <raf.khan.f@gmail.com>
2014-04-25 02:46:24 -0400

    UI Updates, album now shows in background

--- www/app/app.js
+++ www/app/app.js
@@ -18,16 +18,24 @@ angular.module('trance', [])
         artist: 'Armin van Buuren & Andrew Rayel',
         album: 'A State of Trance 2014 - Mixed by Armin van Buuren (In the Club)',
         art:   'assets/img/test/armin_asot.jpg'
+      },
+
+      {
+        name: 'Crazy',
+        artist: 'Gnarls Barkley',
+        album: 'St. Elsewhere',
+        art:   'assets/img/test/st_elsewhere.jpg'
       }
     ];
 
     $scope.current = $scope.tracks[1];
-    $scope.palette = ["red","green"]
+    $scope.palette = ["red","green","blue"]
 
 
     $scope.setTrack = function(track) {
       $scope.current = track;
       var img = document.getElementById('current-art');
+
       var palette = colorThief.getPalette(img, 2);
 
       $scope.palette = _.map(palette, function(color) {
